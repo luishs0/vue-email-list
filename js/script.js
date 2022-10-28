2c016208f8050931d979b4dbbcdf50588b78bcce
luishs0 <luishs.doc@gmail.com>
2022-10-28 14:16:26 +0200

    impostazione fatta

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
   createApp({
     data() {
       return {
-        
+        emails: [],
       }
+    },
+
+    created() {
+        this.getEmail();
+    },
+
+    methods: {
+        getEmail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {console.log(resp.data.response); this.emails.push(resp.data.response)})
+            }
+        }
     }
   }).mount('#app')
